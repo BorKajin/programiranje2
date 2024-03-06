c8bd9b7dab8cc327090502477a88b6c5b65e97fa
Bor Kajin <bor.kajin@gmail.com>
2024-03-06 10:48:46 +0100

    primer Args iz predavanja 2

---
+++ src/predavanja/predavanje02/Args.java
@@ -0,0 +1,10 @@
+package predavanja.predavanje02;
+
+public class Args {
+    public static void main(String[] args) {
+        System.out.printf("Število argumentov: %d\n", args.length);
+        for (int i = 0; i < args.length; i++) {
+            System.out.printf("Argument %2d: %s\n", i+1,args[i]);
+        }
+    }
+}
