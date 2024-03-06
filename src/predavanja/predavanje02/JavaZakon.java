0d291f305d06e5bededfdd3246ed554891512684
Bor Kajin <bor.kajin@gmail.com>
2024-03-06 08:57:35 +0100

    primer JavaZakon iz predavanja 2

---
+++ src/predavanja/predavanje02/JavaZakon.java
@@ -0,0 +1,10 @@
+package predavanja.predavanje02;
+
+public class JavaZakon {
+    public static void main(String[] args) {
+        int steviloPonovitev = 5;
+        for (int i = 1; i <= steviloPonovitev; i++) {
+            System.out.printf("%d. Java je ZAKON!\n", i);
+        }
+    }
+}
