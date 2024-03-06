b6048fcf7eb7f0f9dea58d125b431f6cb7b09402
Bor Kajin <bor.kajin@gmail.com>
2024-03-06 08:39:21 +0100

    domača naloga 1

---
+++ src/naloge/DN01.java
@@ -0,0 +1,14 @@
+package naloge;
+
+public class DN01 {
+    public static void main(String[] args) {
+        System.out.println("""
+                Moje ime je: Bor Kajin
+                Moja vpisna številka: 63230497
+                                
+                ***********************************************
+                *|*  Izjavljam,  da bom vse obveznosti pri  *|*
+                *|*  Programiranju 2 opravil/a samostojno.  *|*
+                ***********************************************""");
+    }
+}
