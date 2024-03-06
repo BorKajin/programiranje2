2b92f90075b56377b163d5039e1ed5bd313efd6b
Bor Kajin <bor.kajin@gmail.com>
2024-03-06 09:20:39 +0100

    primer Srecke iz predavanja 2

---
+++ src/predavanja/predavanje02/Srecke.java
@@ -0,0 +1,12 @@
+package predavanja.predavanje02;
+
+public class Srecke {
+    public static void main(String[] args) {
+        int steviloVrstic = 10;
+        System.out.println("Število srečk | Cena (EUR)");
+        System.out.println("--------------------------");
+        for (int i = 1; i <= steviloVrstic; i++) {
+            System.out.printf("%5d%9s|%6.2f\n",i,"",i*1.25);
+        }
+    }
+}
