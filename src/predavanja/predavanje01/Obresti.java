f7cbf14631df3c8de1fefb18e0319ea5f3ff5fc6
Bor Kajin <bor.kajin@gmail.com>
2024-03-06 08:51:30 +0100

    primer Obresti iz predavanja 1

--- src/predavanja/predavanje01/Obresti.java
+++ src/predavanja/predavanje01/Obresti.java
@@ -1,5 +1,22 @@
 package predavanja.predavanje01;
 
-public class Obresti {
+import java.util.Scanner;
 
+public class Obresti {
+    public static void main(String[] args) {
+        double obrestnaMera;
+        double steviloLet;
+        double glavnica;
+        double koncniZnesek;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Obrestna mera: ");
+        obrestnaMera = sc.nextDouble();
+        System.out.print("Število let: ");
+        steviloLet = sc.nextDouble();
+        System.out.print("Glavnica: ");
+        glavnica = sc.nextDouble();
+        koncniZnesek = glavnica * Math.pow(1+obrestnaMera/100, steviloLet);
+        System.out.println("-----------------------");
+        System.out.printf("Končni znesek: %.10f\n", koncniZnesek);
+    }
 }
