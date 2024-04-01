0b06afecab24d6eb0130fde3b0ee593fe1ee91a7
BoryxKajinyx <71166899+BoryxKajinyx@users.noreply.github.com>
2024-04-01 14:53:15 +0200

    Narejen 3. del DN05

--- src/naloge/DN05.java
+++ src/naloge/DN05.java
@@ -18,6 +18,8 @@ public class DN05 {
             case "rotiraj" -> izpisiSliko(rotirajSliko(preberiSliko(args[1])));
             case "zrcali" -> izpisiSliko(zrcaliSliko(preberiSliko(args[1])));
             case "vrstica" -> izpisiMaxVrstico(poisciMaxVrstico(preberiSliko(args[1])));
+            case "barvna" -> izpisiBarvnoSliko(preberiBarvnoSliko(args[1]));
+            case "sivinska" -> izpisiSliko(pretvoriVSivinsko(preberiBarvnoSliko(args[1])));
         }
 
     }
@@ -156,6 +158,77 @@ public class DN05 {
 
     private static void izpisiMaxVrstico(int vrstica) {
         System.out.printf("Max razlika svetlo - temno je v %d. vrstici.", vrstica);
+    }
+
+    private static int[][][] preberiBarvnoSliko(String ime) {
+        int[][][] slika = null;
+        try (Scanner sc = new Scanner(new File(ime))) {
+            if (!sc.hasNextLine()) {
+                System.out.println("Napaka: Datoteka " + ime + " je prazna.");
+                System.exit(2);
+            }
+            String[] glava = sc.nextLine().split(" ");
+            if (!glava[0].equals("P2B:") || !glava[2].equals("x")) {
+                System.out.println("Napaka: datoteka " + ime + " ni v formatu P2.");
+                System.exit(2);
+            }
+            int sirina = 0, visina = 0;
+            try {
+                sirina = Integer.parseInt(glava[1]);
+                visina = Integer.parseInt(glava[3]);
+            } catch (NumberFormatException e) {
+                System.out.println("Napaka: datoteka " + ime + " ni v formatu P2 (velikost slike ni pravilna).");
+                System.exit(2);
+            }
+            if (sirina <= 0 || visina <= 0) {
+                System.out.println("Napaka: datoteka " + ime + " ni v formatu P2 (velikost slike je 0 ali negativna).");
+                System.exit(2);
+            }
+            slika = new int[sirina][visina][3];
+            for (int i = 0; i < slika[0].length; i++) {
+                for (int j = 0; j < slika.length; j++) {
+                    int naslednji = sc.nextInt();
+                    int maska = 0b1111111111;
+                    slika[j][i][0] = (naslednji >> 20) & maska;
+                    slika[j][i][1] = (naslednji >> 10) & maska;
+                    slika[j][i][2] = naslednji & maska;
+                    if (slika[j][i][2] > 1023 || slika[j][i][2] < 0 ||
+                            slika[j][i][1] > 1023 || slika[j][i][1] < 0 ||
+                            slika[j][i][0] > 1023 || slika[j][i][0] < 0) {
+                        System.out.println("Napaka: datoteka " + ime + " vsebuje podatke izven obsega 0 do 1023.");
+                        System.exit(2);
+                    }
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Napaka: datoteka " + ime + " ne obstaja.");
+            System.exit(2);
+        } catch (NoSuchElementException e) {
+            System.out.println("Napaka: datoteka " + ime + " vsebuje premalo podatkov.");
+            System.exit(2);
+        }
+        return slika;
+    }
+
+    private static void izpisiBarvnoSliko(int[][][] slika) {
+        System.out.printf("velikost slike: %d x %d\n", slika.length, slika[0].length);
+        for (int i = 0; i < slika[0].length; i++) {
+            for (int j = 0; j < slika.length; j++) {
+                System.out.printf("(%4d,%4d,%4d)%s", slika[j][i][0], slika[j][i][1], slika[j][i][2], (j < slika.length - 1) ? " " : "");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] pretvoriVSivinsko(int[][][] slika) {
+        int[][] sivinska = new int[slika.length][slika[0].length];
+        for (int i = 0; i < sivinska.length; i++) {
+            for (int j = 0; j < sivinska[0].length; j++) {
+                sivinska[i][j] = ((slika[i][j][0] + slika[i][j][1] + slika[i][j][2]) / 3) * 255 / 1023;
+            }
+        }
+        return sivinska;
     }
 
 }
